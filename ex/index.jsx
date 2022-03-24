2a2451a371a0ce2df12fbb75afcc1de05dfad94e
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-24 15:07:41 -0300

    Renderiza um componente de classe

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Family from './Family';
-import Member from './Member';
+import ClassComponent from './classComponent';
+
 
 ReactDOM.render(
     <div>
-        <Family lastName={'Silva'}>
-            <Member name={'Rodrigo '} />
-            <Member name={'Ana '} />
-            <Member name={'Samara '} />
-            <Member name={'Carlos '} />
-        </Family>
+        <ClassComponent value={'Componente de classe'} />
     </div>,
     document.getElementById('app')
 )
